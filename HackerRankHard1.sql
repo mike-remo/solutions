72d959cc79def8d45475d663806d85d1f916b2cc
Michael R <142274957+mike-remo@users.noreply.github.com>
2024-06-04 04:09:26 -0700

    Completed solutions for two HackerRank problems

---
+++ HackerRankHard1.sql
@@ -0,0 +1,99 @@
+/*
+Hacker Rank Solution
+By Michael Remollino (mike dot remo @ g mail dot com)
+https://www.hackerrank.com/challenges/interviews/
+*/
+
+-- Sample values for testing
+
+-- INIT database
+CREATE TABLE Contests (
+  contest_id INT,
+  hacker_id INT,
+  name VARCHAR(100)
+);
+CREATE TABLE Colleges (
+  college_id INT,
+  contest_id INT
+);
+CREATE TABLE Challenges (
+  challenge_id INT,
+  college_id INT
+);
+CREATE TABLE View_Stats (
+  challenge_id INT,
+  total_views INT,
+  total_unique_views INT
+);
+CREATE TABLE Submission_Stats (
+  challenge_id INT,
+  total_submissions INT,
+  total_accepted_submissions INT
+);
+
+INSERT INTO Contests(contest_id, hacker_id, name) VALUES
+(66406, 17973, 'Rose'),
+(66556, 79153, 'Angela'),
+(94828, 80275, 'Frank');
+
+INSERT INTO Colleges(college_id, contest_id) VALUES
+(11219, 66406),
+(32473, 66556),
+(56685, 94828);
+
+INSERT INTO Challenges (challenge_id, college_id) VALUES
+(18765, 11219),
+(47127, 11219),
+(60292, 32473),
+(72974, 56685);
+
+INSERT INTO View_Stats (challenge_id, total_views, total_unique_views) VALUES
+(47127, 26, 19),
+(47127, 15, 14),
+(18765, 43, 10),
+(18765, 72, 13),
+(75516, 35, 17),
+(60292, 11, 10),
+(72974, 41, 15),
+(75516, 75, 11);
+
+INSERT INTO Submission_Stats (challenge_id, total_submissions, total_accepted_submissions) VALUES
+(75516, 34, 12),
+(47127, 27, 10),
+(47127, 56, 18),
+(75516, 74, 12),
+(75516, 83, 8),
+(72974, 68, 24),
+(72974, 82, 14),
+(47127, 28, 11);
+
+-- QUERY database
+WITH cte1 AS (
+SELECT cha.challenge_id
+     , SUM(ISNULL(total_views,0)) AS ttl_view
+     , SUM(ISNULL(total_unique_views,0)) AS ttl_unqv
+FROM Challenges cha
+LEFT JOIN View_Stats vie ON vie.challenge_id = cha.challenge_id
+GROUP BY cha.challenge_id
+), cte2 AS (
+SELECT cha.challenge_id
+     , SUM(ISNULL(total_submissions,0)) AS ttl_subm
+     , SUM(ISNULL(total_accepted_submissions,0)) AS ttl_accs
+FROM Challenges cha
+LEFT JOIN Submission_Stats sub ON sub.challenge_id = cha.challenge_id
+GROUP BY cha.challenge_id
+)
+SELECT con.contest_id, hacker_id, name
+     , SUM(ttl_subm) AS ttl_submitted
+     , SUM(ttl_accs) AS ttl_accepted
+     , SUM(ttl_view) AS ttl_views
+     , SUM(ttl_unqv) AS ttl_uq_views
+     --, COUNT(cha.challenge_id) AS hits
+FROM Contests con
+LEFT JOIN Colleges col ON col.contest_id = con.contest_id
+LEFT JOIN Challenges cha ON cha.college_id = col.college_id
+LEFT JOIN cte1 ON cte1.challenge_id = cha.challenge_id
+LEFT JOIN cte2 ON cte2.challenge_id = cha.challenge_id
+GROUP BY con.contest_id, hacker_id, name
+HAVING COUNT(cha.challenge_id) > 0
+ORDER BY con.contest_id ASC;
---
+++ HackerRankHard2.sql
@@ -0,0 +1,114 @@
+/*
+Hacker Rank Solution
+By Michael Remollino (mike dot remo @ g mail dot com)
+https://www.hackerrank.com/challenges/15-days-of-learning-sql/
+*/
+
+-- Sample values for testing
+
+-- INIT database
+CREATE TABLE Submissions (
+  submission_date DATE,
+  submission_id INT,
+  hacker_id INT,
+  score INT
+);
+
+INSERT INTO Submissions(submission_date, submission_id, hacker_id, score) VALUES
+('3/1/2016',8494,20703,0),
+('3/1/2016',22403,53473,15),
+('3/1/2016',23965,79722,60),
+('3/1/2016',30173,36396,70),
+('3/2/2016',34928,20703,0),
+('3/2/2016',38740,15758,80),
+('3/2/2016',42769,79722,25),
+('3/2/2016',44364,79722,60),
+('3/3/2016',45440,20703,0),
+('3/3/2016',49050,36396,70),
+('3/3/2016',50273,79722,5),
+('3/4/2016',50344,20703,0),
+('3/4/2016',51360,44065,90),
+('3/4/2016',54404,53473,65),
+('3/4/2016',61533,79722,45),
+('3/5/2016',72852,20703,0),
+('3/5/2016',74546,38289,0),
+('3/5/2016',76487,62529,0),
+('3/5/2016',82439,36396,10),
+('3/5/2016',90006,36396,40),
+('3/6/2016',90404,20703,0);
+
+-- QUERY database
+
+-- Step 1: Get user w/ most submissiosn per day, tie-breaker goes to lowest id
+WITH cte1 AS (
+SELECT submission_date 
+     , DENSE_RANK() OVER (PARTITION BY submission_date ORDER BY COUNT(submission_id) DESC) AS rank1
+     , DENSE_RANK() OVER (PARTITION BY submission_date ORDER BY hacker_id)AS rank2
+     , hacker_id
+FROM Submissions
+GROUP BY submission_date, hacker_id
+), cte2 AS (
+SELECT submission_date
+     , ROW_NUMBER() OVER (PARTITION BY submission_date ORDER BY rank1, rank2) AS rownum
+     , hacker_id
+FROM cte1
+)
+SELECT submission_date, hacker_id
+FROM cte2
+WHERE rownum = 1
+ORDER BY submission_date ASC;
+
+-- Step 2: Count consecutive login days for each user from start date
+WITH cte1 AS (
+  SELECT DISTINCT submission_date, hacker_id
+  FROM Submissions
+  WHERE submission_date BETWEEN '2016-03-01' AND '2016-03-15'
+), cte2 AS (
+  SELECT submission_date, hacker_id
+  FROM cte1
+  WHERE cte1.submission_date = '2016-03-01'
+  UNION ALL
+  SELECT cte1.submission_date, cte1.hacker_id
+  FROM cte1
+  INNER JOIN cte2 ON cte2.submission_date = DATEADD(dd, -1, cte1.submission_date)
+  AND cte1.hacker_id = cte2.hacker_id
+)
+SELECT submission_date, COUNT(DISTINCT hacker_id) as unqH
+-- ,STRING_AGG(hacker_id, ',') AS unqH
+FROM cte2
+GROUP BY submission_date;
+
+-- Final Combined Query
+WITH cte1 AS (
+SELECT submission_date 
+     , DENSE_RANK() OVER (PARTITION BY submission_date ORDER BY COUNT(submission_id) DESC) AS rank1
+     , DENSE_RANK() OVER (PARTITION BY submission_date ORDER BY hacker_id)AS rank2
+     , hacker_id
+FROM Submissions
+GROUP BY submission_date, hacker_id
+), cte2 AS (
+SELECT submission_date
+     , ROW_NUMBER() OVER (PARTITION BY submission_date ORDER BY rank1, rank2) AS rownum
+     , hacker_id
+FROM cte1
+), cteR AS (
+  SELECT submission_date, hacker_id
+  FROM Submissions sub
+  WHERE sub.submission_date = '2016-03-01'
+  UNION ALL
+  SELECT sub.submission_date, sub.hacker_id
+  FROM Submissions sub
+  INNER JOIN cteR ON cteR.submission_date = DATEADD(dd, -1, sub.submission_date)
+  AND sub.hacker_id = cteR.hacker_id
+  WHERE sub.submission_date < '2016-03-16'
+), cte4 AS (
+  SELECT submission_date, COUNT(DISTINCT hacker_id) as unqH
+  FROM cteR
+  GROUP BY submission_date
+)
+SELECT cte2.submission_date, cte4.unqH, cte2.hacker_id, h.name
+FROM cte2
+LEFT JOIN Hackers h ON cte2.hacker_id = h.hacker_id
+LEFT JOIN cte4 ON cte2.submission_date = cte4.submission_date
+WHERE rownum = 1
+ORDER BY cte2.submission_date;
